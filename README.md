e21a394427b3c273856c824150dc8ba4616f5657
TomasBelskis <tomas.belskis@icloud.com>
2020-08-23 19:12:12 +0100

    Added the new controls and tested

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MacOS Automation
 Basic automation and window management for macos using hammerspoon.
+
+# Window Management Controls
+1. Master Key (Control + Options)
+1.1 Master Key + Left arrow = Move the window to the left half of screen.
+1.2 Master Key + Right arrow =  Move the window to the right half of the screen.
+1.3 Master Key + Arrow up = Move the window to the top half of the screen.
+1.4 Master Key + Arrow down =  Move the window to the bottom half of the screen.
+1.5 Master Key + U = Moves the window to the top left quarter of the screen.
+1.6 Master Key + I = Moves the window to the top right quarter of the screen.
+1.7 Master Key + J = Moves the window to the bottom left of the screen.
+1.8 Master Key + K = Moves the window to the bottom right of the screen.
+1.9 Master key + D = Moves the window to the left third of the screen.
+1.10. Master Key + E = Moves the window to the left two thirds of the screen.
+1.11 Master Key + F = Moves the window to the centre third of the screen.
+1.12 Master key + T = Moves the window to the right to two thirds of the screen.
+1.13 Master Key + G = Moves the window to the right third of the screen.
+1.14 Master Key + Enter = Maximizes the window.
--- ini.lua
+++ ini.lua
@@ -1,3 +1,4 @@
+-- Sleep mdoe disable/enable
 caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
     if state then
@@ -16,10 +17,11 @@ if caffeine then
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
 
-local hyper = {"cmd", "alt", "ctrl"}
+local hyper = {"ctrl","alt"}
 
 hs.window.animationDuration=0
 
+-- Moves the window to the left half of the screen
 hs.hotkey.bind(hyper, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -33,6 +35,7 @@ hs.hotkey.bind(hyper, "Left", function()
   win:setFrame(f)
 end)
 
+-- Moves the window to the right half of the screen
 hs.hotkey.bind(hyper, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -46,6 +49,7 @@ hs.hotkey.bind(hyper, "Right", function()
   win:setFrame(f)
 end)
 
+-- Moves the window to the top half of the screen
 hs.hotkey.bind(hyper, "Up", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -59,6 +63,7 @@ hs.hotkey.bind(hyper, "Up", function()
   win:setFrame(f)
 end)
 
+-- Moves the window to the bottom half of the screen
 hs.hotkey.bind(hyper, "Down", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -72,6 +77,7 @@ hs.hotkey.bind(hyper, "Down", function()
   win:setFrame(f)
 end)
 
+-- Moves the window to the bottom half of the screen
 hs.hotkey.bind(hyper, "Down", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -85,8 +91,8 @@ hs.hotkey.bind(hyper, "Down", function()
   win:setFrame(f)
 end)
 
-
-hs.hotkey.bind(hyper, "i", function()
+-- Moves the window to the top left quarter of the screen
+hs.hotkey.bind(hyper, "u", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -99,7 +105,8 @@ hs.hotkey.bind(hyper, "i", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind(hyper, "o", function()
+-- Moves the window to the top right quarter of the screen
+hs.hotkey.bind(hyper, "i", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -112,7 +119,8 @@ hs.hotkey.bind(hyper, "o", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind(hyper, "k", function()
+-- Moves the window the bottom left quarter of the screen
+hs.hotkey.bind(hyper, "j", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -125,7 +133,8 @@ hs.hotkey.bind(hyper, "k", function()
   win:setFrame(f)
 end)
 
-hs.hotkey.bind(hyper, "l", function()
+-- Moves the window to the bottom right quarter of the screen
+hs.hotkey.bind(hyper, "k", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -135,6 +144,90 @@ hs.hotkey.bind(hyper, "l", function()
   f.y = max.y + (max.h /2)
   f.w = max.w / 2
   f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Maximise the screen
+hs.hotkey.bind(hyper, "return", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = scree:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+ end)
+
+-- Move the window to the left third of the screen
+hs.hotkey.bind(hyper, "d", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max =  screen:frame()
+
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w /3
+	f.h = max.h
+	win:setFrame(f)
+end)
+
+-- Move the window to the centre third of the screen
+hs.hotkey.bind(hyper, "f", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max =  screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move the window to the right third of the screen
+hs.hotkey.bind(hyper, "g", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max =  screen:frame()
+
+  f.x = max.x + 2 * (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move the window to two thirds to the left of the screen
+hs.hotkey.bind(hyper, "e", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max =  screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = 2 * (max.w / 3)
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move the window to two thirds to the right of the screen
+hs.hotkey.bind(hyper, "t", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max =  screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = 2 * (max.w / 3)
+  f.h = max.h
   win:setFrame(f)
 end)
 
